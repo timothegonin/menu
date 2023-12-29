4abe9d9fe330e2eff5684d7d8d66bafe5ae9884c
timotheG <gonin.timothe@gmail.com>
2023-12-29 11:58:15 +0100

    menu selection

--- main.c
+++ main.c
@@ -3,11 +3,33 @@
 
 int main()
 {
+    int userChoice = 0;
+    //INTERFACE
     printf("=== Menu ===\n");
     printf("1. Royal Cheese\n");
     printf("2. Mc Deluxe\n");
     printf("3. Mc Bacon\n");
     printf("4. Big Mac\n");
     printf("Votre choix ?\n");
+    //INPUT
+    scanf("%d", &userChoice);
+    switch(userChoice)
+    {
+    case 1:
+        printf("Vous avez choisi menu le Royal Cheese");
+        break;
+    case 2:
+        printf("Vous avez choisi le menu Mc Deluxe");
+        break;
+    case 3:
+        printf("Vous avez choisi le menu Mc Bacon");
+        break;
+    case 4:
+        printf("Vous avez choisi le menu Big Mac");
+        break;
+    default:
+        printf("Le menu numero %d est invalide...", userChoice);
+        break;
+    }
     return 0;
 }
